dcae9d59d2a13bf97b98b730d63d7716e482be9e
Ricky <ricky.martin2004m@gmail.com>
2026-06-08 02:29:00 +0100

    Adding percentile ranges to make future rules for customer status

--- k-means_clustering.py
+++ k-means_clustering.py
@@ -3,10 +3,28 @@ import pandas as pd
 dataset = pd.read_csv('customer_order_sales_data.csv')
 
 #checking min and max frequency to be able see a good range
-print(dataset['Frequency'].min(), dataset['Frequency'].max())
+#print(dataset['Frequency'].min(), dataset['Frequency'].max())
 
-
+VIP = []
 
 for column in dataset.columns:
-    print(dataset[column])
-    print("Checking min values on all dataset columns: ",dataset[column].min(),"Checking max values on all dataset columns: ", dataset[column].max())
+#    print(dataset[column])
+    print(f"Checking min values on {column} dataset columns: ",dataset[column].min(),f"Checking max values on {column} dataset columns: ", dataset[column].max())
+
+# Create customer limits using quantiles
+# 0.75 means the top 25% of customers
+# 0.25 means the lowest 25% of customers
+
+#using quantile to distribute evenly dataset
+frequency_limit = dataset["Frequency"].quantile(0.75)
+recency_limit = dataset["Recency"].quantile(0.25)
+average_limit = dataset["AverageOrderValue"].quantile(0.75)
+
+#Frequency is in the top 25% because higher frequecny mean more loyalty
+high_frequency = dataset["Frequency"] >= frequency_limit
+
+#Recency is in the bottom 25% because lower recency the sooner they have purchased something
+recent_customer = dataset["Recency"] <= recency_limit
+
+#Average is in the top 25% because higher Average means more spent entirely in store
+high_average = dataset["AverageOrderValue"] >= average_limit
