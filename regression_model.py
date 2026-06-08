4eae37df6930a323bd03049f23d8c5306f724b40
Ricky <ricky.martin2004m@gmail.com>
2026-06-08 16:28:55 +0100

    Splitting our training data into X and our target data into y

--- regression_model.py
+++ regression_model.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
+
 dataset = pd.read_csv("customer_regression_data.csv")
 
 print(dataset.head())
 
+print(dataset.columns)
+
+X_features = ["PreviousMonetaryValue", "PreviousAverageOrderValue", "PreviousOrderCount", "PreviousOrderValue"]
+
+X = dataset[X_features].values
+y = dataset.iloc[:, -1].values
+
+print(X)
+print(y)
