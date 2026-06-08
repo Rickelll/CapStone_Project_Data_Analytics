86c79f347c93d76173a97f36f3f993916aa7575b
Ricky <ricky.martin2004m@gmail.com>
2026-06-08 02:55:16 +0100

    Create customer segment labels

--- k-means_clustering.py
+++ k-means_clustering.py
@@ -19,6 +19,7 @@ for column in dataset.columns:
 frequency_limit = dataset["Frequency"].quantile(0.75)
 recency_limit = dataset["Recency"].quantile(0.25)
 average_limit = dataset["AverageOrderValue"].quantile(0.75)
+high_monetary_value = dataset["MonetaryValue"].quantile(0.25)
 
 #Frequency is in the top 25% because higher frequecny mean more loyalty
 high_frequency = dataset["Frequency"] >= frequency_limit
@@ -28,3 +29,25 @@ recent_customer = dataset["Recency"] <= recency_limit
 
 #Average is in the top 25% because higher Average means more spent entirely in store
 high_average = dataset["AverageOrderValue"] >= average_limit
+
+#Monetary Value is in the top 25& because more overall spending means values customer
+high_monetary_value = dataset["MonetaryValue"] >= high_monetary_value
+
+
+vip_customer = dataset[high_frequency & recent_customer & high_average & high_monetary_value]
+
+loyal_customer = dataset[high_frequency & recent_customer & ~high_average]
+
+at_risk_customer = dataset[high_frequency & ~recent_customer]
+
+new_customer = dataset[~high_frequency & recent_customer]
+
+inactive_customer = dataset[~high_frequency & ~recent_customer]
+
+print("Length of data: ",len(dataset))
+print("VIP customers: ",len(vip_customer))
+print("Loyal Customers: ",len(loyal_customer))
+print("At risk of losing : ",len(at_risk_customer))
+print("New customers: ",len(new_customer))
+print("Inactive Customers: ",len(inactive_customer))
+
