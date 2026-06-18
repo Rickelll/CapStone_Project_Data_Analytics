d2fdfc1b9bc053b06e53906688d26a4a0e23f0e3
Ricky <ricky.martin2004m@gmail.com>
2026-06-18 21:00:31 +0100

    Created Inspect feature for Customer Order Search

--- Streamlit/streamlit_gui.py
+++ Streamlit/streamlit_gui.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
+from datetime import datetime as dt
 
 
 st.sidebar.header('Customer Segmentation Analysis')
@@ -38,7 +39,7 @@ def home_page():
 
 
 def search_customer_order():
-    st.title('Customer Order Search')
+    st.title('Invoice Lookup')
 
     purchased_orders, cancelled_orders = load_order_data()
 
@@ -64,14 +65,48 @@ def search_customer_order():
 
             st.write("Order Details: ")
 
+            display_df = matching_code[
+                [
+                    "InvoiceNo",
+                    "CustomerID",
+                    "InvoiceDate",
+                    "Country",
+                    "Description",
+                    "Quantity",
+                    "UnitPrice",
+                    "Order_Value"
+                ]
+            ].copy().reset_index(drop=True)
 
+            # Format date BEFORE showing the dataframe
+            display_df["InvoiceDate"] = pd.to_datetime(display_df["InvoiceDate"])
+            display_df["InvoiceDate"] = display_df["InvoiceDate"].dt.strftime("%d/%m/%Y")
 
-            dataframe = st.dataframe(matching_code[['InvoiceNo','CustomerID','InvoiceDate','Country',
-                                   'Description', 'Quantity', 'UnitPrice','Order_Value']].head(5))
-            st.write(dataframe)
+            table_event = st.dataframe(
+                display_df,
+                use_container_width=True,
+                hide_index=True,
+                on_select="rerun",
+                selection_mode="single-row"
+            )
 
-        else:
-            st.error(f'No Order Found for Invoice Number: {invoice_no}')
+            selected_row = table_event.selection.rows
+
+            if selected_row:
+                selected_row_number = selected_row[0]
+                selected_data = display_df.iloc[selected_row_number]
+
+                inspect_button = st.button("Inspect")
+
+                if inspect_button:
+                    st.write(
+                        f"This order was placed on {selected_data['InvoiceDate']}. \n"
+                        f"The customer bought {selected_data['Quantity']} of \n"
+                        f"{selected_data['Description']} at €{selected_data['UnitPrice']:.2f} per item, \n"
+                        f"giving this Order a value of €{selected_data['Order_Value']:.2f}."
+                    )
+            else:
+                st.info("Click one row in the table, then press Inspect.")
 
 if __name__ == '__main__':
     if selected=="Home":
