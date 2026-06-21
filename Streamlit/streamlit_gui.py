0916c8e42f1fa6f46f6a8664ebda0beec8177a60
Ricky <ricky.martin2004m@gmail.com>
2026-06-21 01:59:30 +0100

    Created Bottom Row Chart

--- Streamlit/streamlit_gui.py
+++ Streamlit/streamlit_gui.py
@@ -61,6 +61,11 @@ monthly_completed_cancelled_revenue = """
     vizElement.parentNode.insertBefore(scriptElement, vizElement);
 </script>
 """
+
+top10_countries_by_completed_revenue = '''<div class='tableauPlaceholder' id='viz1782003023595' style='position: relative'><noscript><a href='#'><img alt='Top Countries by Completed Revenue ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;TopCountriesbycompletedRevenueBarChart&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Capstone_Project_Code-Institue_backup&#47;TopCountriesbycompletedRevenueBarChart' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;TopCountriesbycompletedRevenueBarChart&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1782003023595');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>'''
+
+top10_products_by_completed_revenue = '''<div class='tableauPlaceholder' id='viz1782003113916' style='position: relative'><noscript><a href='#'><img alt='Which products produce the most revenue? ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;TopProductsbyRevenueBarChart&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Capstone_Project_Code-Institue_backup&#47;TopProductsbyRevenueBarChart' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;TopProductsbyRevenueBarChart&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1782003113916');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>'''
+
 st.sidebar.header('Customer Segmentation Analysis')
 st.set_page_config(
     page_title="Capstone Dashboard",
@@ -285,10 +290,23 @@ def sales_report():
     with col6:
         components.html(total_invoices_kpi, height=220, scrolling=False)
 
+    st.divider()
+
     st.subheader("MAIN CHART")
 
     components.html(monthly_completed_cancelled_revenue, height=700, scrolling=False)
 
+    st.divider()
+
+    st.subheader("BOTTOM ROW CHART")
+
+    col7, col8 = st.columns(2)
+
+    with col7:
+        components.html(top10_countries_by_completed_revenue, height=500, scrolling=False)
+
+    with col8:
+        components.html(top10_products_by_completed_revenue, height=500, scrolling=False)
 
 if __name__ == '__main__':
     if selected=="Home":
