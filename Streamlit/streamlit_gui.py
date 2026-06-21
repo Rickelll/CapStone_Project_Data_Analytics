10267f08364640ef2bb9de9221df213659cf6a18
Ricky <ricky.martin2004m@gmail.com>
2026-06-21 03:04:47 +0100

    Created Charts for customer segmentation

--- Streamlit/streamlit_gui.py
+++ Streamlit/streamlit_gui.py
@@ -32,6 +32,10 @@ loyal_customers_kpi = '''<div class='tableauPlaceholder' id='viz1782006389496' s
 
 at_risk_customers_kpi = '''<div class='tableauPlaceholder' id='viz1782006400525' style='position: relative'><noscript><a href='#'><img alt='At Risk Customers ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;AtRiskCustomers&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Capstone_Project_Code-Institue_backup&#47;AtRiskCustomers' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;AtRiskCustomers&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1782006400525');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>'''
 
+customer_group_total_value = '''<div class='tableauPlaceholder' id='viz1782006953164' style='position: relative'><noscript><a href='#'><img alt='Which Customer Group Generated the Most Total Value? ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;CustomerGroups-TotalValueVertical&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Capstone_Project_Code-Institue_backup&#47;CustomerGroups-TotalValueVertical' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;CustomerGroups-TotalValueVertical&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1782006953164');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>'''
+
+customer_group_total_per_customer = '''<div class='tableauPlaceholder' id='viz1782007023706' style='position: relative'><noscript><a href='#'><img alt='Which customer group is most valuable per customer? ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;CustomerGroups-AverageValueVertical&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Capstone_Project_Code-Institue_backup&#47;CustomerGroups-AverageValueVertical' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;CustomerGroups-AverageValueVertical&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1782007023706');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>'''
+
 st.sidebar.header('Customer Segmentation Analysis')
 st.set_page_config(
     page_title="Capstone Dashboard",
@@ -318,7 +322,7 @@ def customer_segmentation():
     st.divider()
 
     centered_subheader("Small explanation")
-    st.write('Customers were grouped using frequency, recency, average order value, and monetary value.')
+    centered_text('Customers were grouped using frequency, recency, average order value, and monetary value.')
 
     st.divider()
 
@@ -343,6 +347,21 @@ def customer_segmentation():
     with right:
         with col4:
             components.html(at_risk_customers_kpi, height=220, scrolling=False)
+
+    st.divider()
+
+    centered_subheader("Charts")
+
+    col5, col7 = st.columns(2)
+    left,right = st.columns([1,1])
+
+    with left:
+        with col5:
+            components.html(customer_group_total_value, height=600, scrolling=False)
+
+    with right:
+        with col7:
+            components.html(customer_group_total_per_customer, height=600, scrolling=False)
 
     st.divider()
 
