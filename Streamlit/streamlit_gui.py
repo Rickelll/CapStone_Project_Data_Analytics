6db9dcf0a2f7e6537477a19702315f55800466fd
Ricky <ricky.martin2004m@gmail.com>
2026-06-21 01:44:15 +0100

    Created Main Chart for sales report

--- Streamlit/streamlit_gui.py
+++ Streamlit/streamlit_gui.py
@@ -18,8 +18,49 @@ average_invoices_kpi = '''<div class='tableauPlaceholder' id='viz1782001711676' 
 
 total_invoices_kpi = '''<div class='tableauPlaceholder' id='viz1782001655034' style='position: relative'><noscript><a href='#'><img alt='Total Invoices ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;TotalInvoices&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Capstone_Project_Code-Institue_backup&#47;TotalInvoices' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;TotalInvoices&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1782001655034');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>'''
 
-monthly_completed_cancelled_revenue = '''<div class='tableauPlaceholder' id='viz1782001969808' style='position: relative'><noscript><a href='#'><img alt='Monthly Revenue vs Cancellation Loss ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;RevenuevsCancellationLoss&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Capstone_Project_Code-Institue_backup&#47;RevenuevsCancellationLoss' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ca&#47;Capstone_Project_Code-Institue_backup&#47;RevenuevsCancellationLoss&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1782001969808');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>'''
+monthly_completed_cancelled_revenue = """
+<div class='tableauPlaceholder' id='monthlyRevenueCancellationViz' style='position: relative; width: 100%;'>
+    <noscript>
+        <a href='#'>
+            <img 
+                alt='Monthly Revenue vs Cancellation Loss'
+                src='https://public.tableau.com/static/images/Ca/Capstone_Project_Code-Institue_backup/RevenuevsCancellationLoss/1_rss.png'
+                style='border: none; width: 100%;'
+            />
+        </a>
+    </noscript>
 
+    <object class='tableauViz' style='display:none; width:100%; height:650px;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+        <param name='embed_code_version' value='3' />
+        <param name='site_root' value='' />
+        <param name='name' value='Capstone_Project_Code-Institue_backup/RevenuevsCancellationLoss' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='no' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Ca/Capstone_Project_Code-Institue_backup/RevenuevsCancellationLoss/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='no' />
+        <param name='language' value='en-GB' />
+        <param name='filter' value='publish=yes' />
+    </object>
+</div>
+
+<script type='text/javascript'>
+    var divElement = document.getElementById('monthlyRevenueCancellationViz');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+
+    vizElement.style.width = '100%';
+    vizElement.style.height = '650px';
+
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+</script>
+"""
 st.sidebar.header('Customer Segmentation Analysis')
 st.set_page_config(
     page_title="Capstone Dashboard",
@@ -244,16 +285,10 @@ def sales_report():
     with col6:
         components.html(total_invoices_kpi, height=220, scrolling=False)
 
+    st.subheader("MAIN CHART")
 
+    components.html(monthly_completed_cancelled_revenue, height=700, scrolling=False)
 
-    st.subheader("Full Tableau Sales Dashboard")
-
-    components.html(
-        sales_dashboard_embed_code,
-        height=1100,
-        width=1600,
-        scrolling=True
-    )
 
 if __name__ == '__main__':
     if selected=="Home":
